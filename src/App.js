ab8efc6d65360eabde22b646f6ea64d9539ddbb2
Andrew Stern <andrewdstern16@gmail.com>
2022-03-20 15:07:11 -0600

    Enter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter,
+  Link, 
+  Switch, 
+  Route
+} from "react-router-dom";
+import Home from "./components/Home"
+import About from "./components/About"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <h1>Routing Example</h1>
+      <p>
+        <Link to="/">Home</Link>
+        |
+        <Link to="/about">About</Link>
+      </p>
+      <Switch>
+        <Route exact path="/about" component={About}>
+          <About/>
+        </Route>
+        <Route exact path="/" component={Home}>
+          <Home/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const About = (props) => {
+    return (
+        <h1>Read about...</h1>
+    )
+}
+
+export default About;
